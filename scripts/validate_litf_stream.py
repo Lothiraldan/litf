99e6c7f3c4f5ed72cd05580d4d43de290bea80c1
Boris Feld <lothiraldan@gmail.com>
2020-08-31 23:35:33 +0200

    Update validate_litf_stream to check also for messages FSM
    Add missing LICENSE file

--- scripts/validate_litf_stream.py
+++ scripts/validate_litf_stream.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+# Copyright (c) 2020 Boris Feld
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
 """ Test runner for validating LITF plugins
 """
 import argparse
@@ -9,7 +32,16 @@ from os.path import abspath, dirname, join
 from jsonschema import ValidationError, validate
 
 current_dir = dirname(abspath(__file__))
-script_dir = join(current_dir, '../spec')
+script_dir = join(current_dir, "../spec")
+
+VALID_TRANSITIONS = {
+    None: ["litf_start"],
+    "litf_start": ["session_start"],
+    "session_start": ["test_collection", "test_result", "session_end"],
+    "test_collection": ["test_collection", "session_end"],
+    "test_result": ["test_result", "session_end"],
+    "session_end": [],
+}
 
 
 def test(stream):
@@ -17,6 +49,8 @@ def test(stream):
     warnings = []
     total_lines = 0
     valid_lines = 0
+
+    current_state = None
 
     # Check stdout
     for line in stream.readlines():
@@ -34,7 +68,7 @@ def test(stream):
         try:
             data = json.loads(line)
         except (json.JSONDecodeError, ValueError) as e:
-            errors.append(("Invalid JSON line", line))
+            errors.append(("Invalid JSON line", line, str(e)))
             continue
 
         # Get the msg type
@@ -44,10 +78,21 @@ def test(stream):
             errors.append(("Missing msg_type", line))
             continue
 
+        # Check that the transition is correct
+        if msg_type not in VALID_TRANSITIONS[current_state]:
+            errors.append(
+                (
+                    "Invalid transition",
+                    line,
+                    "%r is not a valid transition from %r" % (msg_type, current_state),
+                )
+            )
+            continue
+
         # Get the schema
         schema_path = os.path.join(script_dir, msg_type + ".json")
 
-        with open(schema_path, 'r') as schema_file:
+        with open(schema_path, "r") as schema_file:
             schema = json.load(schema_file)
 
         # Validate the line
@@ -57,8 +102,7 @@ def test(stream):
             errors.append(("Invalid data", line, str(exc)))
             continue
 
-        # Validate the logic
-        # TODO
+        current_state = msg_type
 
         valid_lines += 1
 
@@ -84,7 +128,10 @@ def test(stream):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        "stream", help="Which file to read, use - for reading stdin instead", action="store", default="-"
+        "stream",
+        help="Which file to read, use - for reading stdin instead",
+        action="store",
+        default="-",
     )
 
     args = parser.parse_args()
